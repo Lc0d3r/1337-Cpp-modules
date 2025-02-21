5da50e9158a487f11096ac7413419ca9dab17ff5
younesscoder01 <younesscoder@gmail.com>
2025-02-21 16:31:34 +0100

    done with ex05

--- Module_01/ex05/Harl.cpp
+++ Module_01/ex05/Harl.cpp
@@ -10,22 +10,24 @@ void Harl::info() {
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
-void Harl::error() {
+void Harl::warning() {
     std::cout << "[ WARNING ]"<< std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
-void Harl::warning() {
+void Harl::error() {
     std::cout << "[ ERROR ]"<< std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
-void Harl::complain( std::string level ){
+void Harl::complain(std::string level){
 
-    void (Harl::*ptr)();
-    ptr = &Harl::debug;
-    info();
-    error();
-    warning();
-    (void)level;
+    void (Harl::*fptr[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    std::string complains[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    int i = 0;
+    while (level != complains[i] && i < 4)
+        i++;
+    while (i >= 4)
+        return ;
+    (this->*fptr[i])();
 }
--- Module_01/ex05/main.cpp
+++ Module_01/ex05/main.cpp
@@ -3,6 +3,10 @@
 int main(){
     Harl zfat;
 
+    zfat.complain("ERROR");
+    zfat.complain("WARNING");
+    zfat.complain("DEBUG");
     zfat.complain("INFO");
+    zfat.complain("gg");
     return 0;
 }
