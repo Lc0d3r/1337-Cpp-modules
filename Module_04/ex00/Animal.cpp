960b325b4804607896224f69921f2c10f68e0372
younesscoder01 <younesscoder@gmail.com>
2025-04-19 11:34:19 +0100

    almost done with the ex00

---
+++ Module_04/ex00/Animal.cpp
@@ -0,0 +1,26 @@
+#include "Animal.hpp"
+
+Animal::Animal(): type("Animal") {};
+
+Animal::Animal(const Animal& other) {
+    *this = other;
+}
+
+Animal::~Animal() {};
+
+Animal& Animal::operator=(const Animal& other) {
+    this->type = other.type;
+    return *this;
+}
+
+std::string Animal::getType() const {
+    return type;
+}
+
+void Animal::setType(std::string _type) {
+    type = _type;
+}
+
+void Animal::makeSound() const {
+    std::cout << "An animale sound" << std::endl ;
+}
