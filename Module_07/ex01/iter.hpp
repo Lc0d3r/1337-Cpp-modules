cfdd0ce9450496bf8b2eb9ec7572bf30990f65d5
Lc0d3r <younesscoder@gmail.com>
2025-11-14 23:35:39 +0100

    simple edit in ex01 module 07

--- Module_07/ex01/iter.hpp
+++ Module_07/ex01/iter.hpp
@@ -1,15 +1,15 @@
 #pragma once 
 #include <cstddef>
 
-template <typename T_array, typename T_function>
-void iter(T_array* array, size_t length, T_function func) {
+template <typename T_array>
+void iter(T_array* array, size_t length, void (*func)(T_array&)) {
     for (size_t i = 0; i < length; ++i) {
         func(array[i]);
     }
 }
 
-template <typename T_array, typename T_function>
-void iter(const T_array* array, size_t length, T_function func) {
+template <typename T_array>
+void iter(const T_array* array, size_t length, void (*func)(const T_array&)) {
     for (size_t i = 0; i < length; ++i) {
         func(array[i]);
     }
