9f91adbc647f4c4da50949b64c6e0b2e71559faf
Lc0d3r <younesscoder@gmail.com>
2025-11-14 23:36:11 +0100

    simple edit in ex00 module 08

--- Module_08/ex00/easyfind.hpp
+++ Module_08/ex00/easyfind.hpp
@@ -3,17 +3,11 @@
 #include <algorithm>
 #include <exception>
 
-class ElementNotFoundException : public std::exception {
-    virtual const char* what() const throw() {
-        return "ElementNotFoundException";
-    }
-};
-
 template <typename T>
 int easyfind(T& data, int toFind) {
     typename T::iterator it = std::find(data.begin(), data.end(), toFind);
 
     if (it == data.end())
-        throw ElementNotFoundException();
+        throw std::out_of_range("Value not found");
     return *it;
 }
